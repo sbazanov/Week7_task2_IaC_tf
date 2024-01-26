10c3b5b74576142804e6f68a0ec2be6ea2bb2270
Serhii Bazanov <96147501+sbazanov@users.noreply.github.com>
2024-01-26 11:35:46 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,20 @@ variable "GOOGLE_PROJECT" {
   default     = ""
   description = "GCP project to use"
 }
+
+variable "GOOGLE_REGION" {
+  type        = string
+  default     = ""
+  description = "GCP region to use"
+}
+
+variable "GKE_NUM_NODES" {
+  type        = number
+  default     = "2"
+  description = "Number of nodes"
+}
+
+variable "GKE_CLUSTER_NAME" {
+  type        = string
+  default     = ""
+  description = "GKE cluster name"
